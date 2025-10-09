b7c92cbc709bb00d65c302f86fe14a2cb0c1518b
Yan Konev <yan09.11.2006@gmail.com>
2025-10-10 00:38:45 +0400

    create interface MathFunction

---
+++ laba2/src/functions/IdentityFunctionTest.java
@@ -0,0 +1,29 @@
+package functions;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class IdentityFunctionTest {
+
+    @org.junit.jupiter.api.Test
+    void apply_1equals1() {
+        for (int i = 0; i < 1000; i++) {
+            IdentityFunction x = new IdentityFunction();
+
+            double result = x.apply(1.0);
+
+            assertEquals(1.0, result);
+        }
+    }
+
+    @Test
+    void apply_2equals2() {
+
+            IdentityFunction x = new IdentityFunction();
+
+            double result = x.apply(2.3);
+
+            assertEquals(2.3, result);
+        }
+    }
