e54bef23d0c86a1cf10cb12b01e7c192689f9c3a
roshchina-anna <roshchina.anna2@mail.ru>
2025-11-18 02:24:59 +0400

    writeTabulatedFunction +  TabulatedFunctionFileWriter

--- laba2/src/io/FunctionsIO.java
+++ laba2/src/io/FunctionsIO.java
@@ -1,6 +1,21 @@
 package io;
+
+import functions.TabulatedFunction;
+import functions.Point;
+import java.io.*;
+
 public final class FunctionsIO {
     private FunctionsIO() {
         throw new UnsupportedOperationException();
+    }
+    public static void writeTabulatedFunction(BufferedWriter writer, TabulatedFunction function) throws IOException {
+        PrintWriter printWriter = new PrintWriter(writer);
+        // записываем количество точек
+        printWriter.println(function.getCount());
+        // записываем точки в цикле for-each
+        for (Point point : function) {
+            printWriter.printf("%f %f\n", point.x, point.y);
+        }
+        printWriter.flush();
     }
 }
---
+++ laba2/src/io/TabulatedFunctionFileWriter.java
@@ -0,0 +1,31 @@
+package io;
+
+import functions.TabulatedFunction;
+import functions.ArrayTabulatedFunction;
+import functions.LinkedListTabulatedFunction;
+import java.io.*;
+
+public class TabulatedFunctionFileWriter {
+    public static void main(String[] args) {
+        double[] xValues = {1.0, 2.0, 3.0, 4.0, 5.0};
+        double[] yValues = {1.0, 4.0, 9.0, 16.0, 25.0};
+
+        // создаем две табулированные функции
+        TabulatedFunction arrayFunction = new ArrayTabulatedFunction(xValues, yValues);
+        TabulatedFunction linkedListFunction = new LinkedListTabulatedFunction(xValues, yValues);
+
+        try (
+                FileWriter arrayFileWriter = new FileWriter("output/array function.txt");
+                FileWriter linkedListFileWriter = new FileWriter("output/linked list function.txt");
+                BufferedWriter arrayBufferedWriter = new BufferedWriter(arrayFileWriter);
+                BufferedWriter linkedListBufferedWriter = new BufferedWriter(linkedListFileWriter)
+        ) {
+            FunctionsIO.writeTabulatedFunction(arrayBufferedWriter, arrayFunction);
+            FunctionsIO.writeTabulatedFunction(linkedListBufferedWriter, linkedListFunction);
+            System.out.println("Functions have been successfully written to files");
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
