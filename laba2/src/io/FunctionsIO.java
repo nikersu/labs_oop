205d88e31737c1d327ff7372ad09fe356d86169f
roshchina-anna <roshchina.anna2@mail.ru>
2025-11-18 02:25:29 +0400

    Revert "writeTabulatedFunction +  TabulatedFunctionFileWriter"
    This reverts commit e54bef23d0c86a1cf10cb12b01e7c192689f9c3a.

--- laba2/src/io/FunctionsIO.java
+++ laba2/src/io/FunctionsIO.java
@@ -1,21 +1,6 @@
 package io;
-
-import functions.TabulatedFunction;
-import functions.Point;
-import java.io.*;
-
 public final class FunctionsIO {
     private FunctionsIO() {
         throw new UnsupportedOperationException();
-    }
-    public static void writeTabulatedFunction(BufferedWriter writer, TabulatedFunction function) throws IOException {
-        PrintWriter printWriter = new PrintWriter(writer);
-        // записываем количество точек
-        printWriter.println(function.getCount());
-        // записываем точки в цикле for-each
-        for (Point point : function) {
-            printWriter.printf("%f %f\n", point.x, point.y);
-        }
-        printWriter.flush();
     }
 }
