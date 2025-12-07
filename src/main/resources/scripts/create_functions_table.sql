bb80e84fe3a36161724e970a709495f6fe475d3f
roshchina-anna <roshchina.anna2@mail.ru>
2025-12-07 22:01:02 +0400

    fix

--- src/main/resources/scripts/create_functions_table.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE functions (
-    id SERIAL PRIMARY KEY,
-    name VARCHAR(100) NOT NULL,
-    expression TEXT NOT NULL,
-    user_id INTEGER NOT NULL,
-    FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
-);
